0012238373f1d2f94ced438d60b571676ee27341
asbel05 <asbelcristobalavila@gmail.com>
2026-06-20 00:52:37 -0500

    chore: estructura inicial de la solucion en .NET 8

---
+++ src/InventarioVentas.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventarioVentas.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
